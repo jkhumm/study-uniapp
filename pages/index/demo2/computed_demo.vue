f747eab68e038101effa3a0b5b32500c3e15abd1
humingming <1158139789@qq.com>
2024-05-05 23:32:59 +0800

    #update

---
+++ pages/index/demo2/computed_demo.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="out">
+		<input type="text" v-model="firstName" placeholder="请输入名" />
+		<input type="text" v-model="lastName" placeholder="请输入姓氏" />
+		<!-- 		<view>全称：{{firstName + "-" + lastName}}</view> -->
+		<view>全称：{{fullName}}</view>
+		<!-- <view>全称：{{fullName()}}</view> -->
+	</view>
+</template>
+
+<script setup>
+	import {
+		computed,
+		ref
+	} from 'vue';
+	const firstName = ref("")
+	const lastName = ref("")
+
+	// 也可以定义一个计算函数 写法是()=> {return a+b} 一行时可以缩写 ()=>a+b
+	//  第二种写法：有缓存，当存在多个一样的view时，只会计算一次，性能好
+	const fullName = computed(() => firstName.value + "-" + lastName.value)
+
+	// 第三种写法：函数方式，但是每次计算式都会被调用
+	// 	const fullName = () => firstName.value + "-" + lastName.value
+</script>
+
+<style lang="scss" scoped>
+	.out {
+		padding: 20px;
+
+		input {
+			border: 1px solid #eee;
+			height: 40px;
+			margin: 10px 0; // 上下10 左右0
+		}
+	}
+</style>
---
+++ pages/index/demo2/watch_demo.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<input type="text" v-model="person.name" />
+
+	</view>
+	{{person}}
+</template>
+
+<script setup>
+	import {
+		ref,
+		watch
+	} from 'vue';
+	const person = ref({
+		name: '张三',
+		age: 10
+	})
+
+	// 默认浅层监听，比如绑定的是input的字符串的值
+	// watch(person, (newValue, oldValue) => {
+	// 	console.log(newValue)
+	// 	console.log(oldValue)
+	// })
+
+	// 如果是对象需要使用箭头函数
+	// watch(() => person.value.name, (newValue, oldValue) => {
+	// 	console.log(newValue)
+	// 	console.log(oldValue)
+	// })
+
+	// 深度监听
+	watch(person, (newValue) => {
+		console.log(newValue)
+	}, {
+		deep: true,
+		immediate: true
+	})
+</script>
+
+<style>
+
+</style>
