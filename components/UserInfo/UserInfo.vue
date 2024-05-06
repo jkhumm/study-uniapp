6fe45e63e4c95bdb5753826e0a90c73369258e99
humingming <1158139789@qq.com>
2024-05-07 00:06:32 +0800

    #update

--- components/UserInfo/UserInfo.vue
+++ components/UserInfo/UserInfo.vue
@@ -1,42 +1,66 @@
 <template>
 	<view class="userinfo">
-		<image :src="touxiang" mode="" class="avatar"></image>
+		<image :src="obj.touxiang" mode="" class="avatar"></image>
 		<view class="username">
-			{{xingming}}
+			{{obj.xingming}}
 		</view>
 	</view>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-const prop =defineProps(['touxiang','xingming'])
-// console.log(prop.xingming) 因为父传到子组件是只读的，单向数据流
+	import {
+		computed
+	} from 'vue';
+	// 写法一
+	// const prop = defineProps(['touxiang', 'xingming'])
+	// 写法二
+	// const prop = defineProps({
+	// 	touxiang: String,
+	// 	xingming: {
+	// 		type: String,
+	// 		default: "胡明明"
+	// 	}
+	// })
+	// 写法三
+	defineProps({
+		obj: {
+			type: Object,
+			// 如果默认值是数组或者对象，要写一个函数
+			default () {
+				return {
+					touxiang: "https://www.xfh2022.com/api/admin/file/download?id=24",
+					xingming: "胡明明设置的默认值"
+				}
+			}
+		}
+	})
 
-// const myName = computed(()=> prop.xingming + "@xxx")
-// console.log(myName.value)
+	// console.log(prop.xingming) 因为父传到子组件是只读的，单向数据流
 
+	// const myName = computed(()=> prop.xingming + "@xxx")
+	// console.log(myName.value)
 </script>
 
 // scoped css仅限在组建内部使用，防止污染其它组件
-<style lang="scss" scoped> 
-.userinfo{
-	width: 100%;
-	height: 100%;
-	background: #ccc;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	image{
-		width: 100px;
-		height: 100px;
-		border-radius: 50%;
-	}
-	.username{
-		padding: 10px 0;
-		font-size: 20px;
-	}
-}
+<style lang="scss" scoped>
+	.userinfo {
+		width: 100%;
+		height: 100%;
+		background: #ccc;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
 
+		image {
+			width: 100px;
+			height: 100px;
+			border-radius: 50%;
+		}
 
+		.username {
+			padding: 10px 0;
+			font-size: 20px;
+		}
+	}
 </style>
--- pages/index/demo2/watch_demo.vue
+++ pages/index/demo2/watch_demo.vue
@@ -3,13 +3,14 @@
 		<input type="text" v-model="person.name" />
 	</view>
 	{{person}}
-	
+
+
 	<view>
-		<UserInfo xingming="胡三刀" touxiang="https://www.xfh2022.com/api/admin/file/download?id=26"></UserInfo>
-		<UserInfo xingming="胡三刀" touxiang="https://www.xfh2022.com/api/admin/file/download?id=24"></UserInfo>
-		<UserInfo :xingming=person.name touxiang="https://www.xfh2022.com/api/admin/file/download?id=25"></UserInfo>
+		<UserInfo v-for="(item,index) in userInfoObj" :obj="item" :key="item.xingming"></UserInfo>
+
+
 	</view>
-	
+
 </template>
 
 <script setup>
@@ -22,21 +23,36 @@
 		age: 10
 	})
 
-	// 默认浅层监听，比如绑定的是input的字符串的值
-	// watch(person, (newValue, oldValue) => {
-	// 	console.log(newValue)
-	// 	console.log(oldValue)
+	// 定义组件传值的类型
+	const userInfoObj = ref(
+		[{
+			touxiang: "https://www.xfh2022.com/api/admin/file/download?id=24",
+			xingming: "张三"
+		}, {
+			touxiang: "https://www.xfh2022.com/api/admin/file/download?id=25",
+			xingming: "李四"
+		}, {
+			touxiang: "https://www.xfh2022.com/api/admin/file/download?id=26",
+			// xingming: "11"
+		}]
+	)
+
+
+	// 默认浅层监听，比如绑定的是input的字符串的值 
+	//	watch(person, (newValue, oldValue) => {
+	// console.log(newValue)
+	// console.log(oldValue)
 	// })
 
 	// 如果是对象需要使用箭头函数
 	// watch(() => person.value.name, (newValue, oldValue) => {
-	// 	console.log(newValue)
-	// 	console.log(oldValue)
+	// console.log(newValue)
+	// console.log(oldValue)
 	// })
 
 	// 深度监听
 	watch(person, (newValue) => {
-		console.log(newValue)
+		// console.log(newValue)
 	}, {
 		deep: true,
 		immediate: true
