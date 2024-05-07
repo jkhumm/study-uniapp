07b7a1eceb950d86e0df335ec6f21514daedae24
humingming <1158139789@qq.com>
2024-05-07 11:35:06 +0800

    #fix conflict

--- components/UserInfo/UserInfo.vue
+++ components/UserInfo/UserInfo.vue
@@ -1,20 +1,21 @@
 <template>
 	<view class="userinfo">
-<<<<<<< HEAD
-		<image :src="obj.touxiang" mode="" class="avatar"></image>
+
+		<image :src="obj.touxiang" class="avatar"></image>
 		<view class="username">
 			{{obj.xingming}}
-=======
-		<image :src="touxiang" mode="" class="avatar"></image>
+		</view>
+		
+	<!-- 	<image :src="touxiang" mode="" class="avatar"></image>
 		<view class="username">
 			{{xingming}}
->>>>>>> a01c89af711bd3e1b5dce3790eed83967196ecff
-		</view>
+		</view> -->
+		
 	</view>
 </template>
 
 <script setup>
-<<<<<<< HEAD
+
 	import {
 		computed
 	} from 'vue';
@@ -48,37 +49,6 @@
 	// console.log(myName.value)
 </script>
 
-// scoped css仅限在组建内部使用，防止污染其它组件
-<style lang="scss" scoped>
-	.userinfo {
-		width: 100%;
-		height: 100%;
-		background: #ccc;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-
-		image {
-			width: 100px;
-			height: 100px;
-			border-radius: 50%;
-		}
-
-		.username {
-			padding: 10px 0;
-			font-size: 20px;
-		}
-	}
-=======
-import { computed } from 'vue';
-const prop =defineProps(['touxiang','xingming'])
-// console.log(prop.xingming) 因为父传到子组件是只读的，单向数据流
-
-// const myName = computed(()=> prop.xingming + "@xxx")
-// console.log(myName.value)
-
-</script>
 
 // scoped css仅限在组建内部使用，防止污染其它组件
 <style lang="scss" scoped> 
@@ -102,5 +72,5 @@ const prop =defineProps(['touxiang','xingming'])
 }
 
 
->>>>>>> a01c89af711bd3e1b5dce3790eed83967196ecff
+
 </style>
