3527d660f846525da81904ca3414b71483cccb5d
humingming <1158139789@qq.com>
2024-05-07 18:48:27 +0800

    #update

---
+++ components/xxm-child/xxm-child.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		子组件
+<!-- 		<button class="button" @click="$emit('customerColor', Math.random())">按钮</button> -->
+		
+		<button class="button" @click="onClick">按钮</button>
+		
+		
+		<input type="text" @input="onInput" />
+		
+	</view>
+</template>
+
+<script setup>
+	
+	const emitDemo = defineEmits(['customerColor','customerChange'])
+	
+	function onClick() {
+		emitDemo("customerColor", Math.random())
+	}
+	
+	function onInput(e) {
+		emitDemo("customerChange", e.detail.value)
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+.button{
+	font-size: 12px;
+	width: 100px;
+	height: 30px;
+	background-color: gray;
+	margin-left: 0px;
+}
+input{
+	border: 1px solid #cfcfcf;
+	height: 40px;
+}
+</style>
---
+++ pages/index/demo3/child_demo/child_demo.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<xxm-child @customerColor="changeColor" @customerChange="writeInput"></xxm-child>
+		<view class="box" :style="{background:color}"></view>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+	
+	const num = ref(0)
+	const color = ref("#ccc")
+	function changeColor(e){
+		num.value = e;
+		color.value = "#" + String(e).substring(3,6)
+		console.log(color.value)
+	}
+	
+	function writeInput(e){
+		console.log(e)
+	}
+	
+	
+</script>
+
+<style lang="scss">
+.box{
+	width: 200px;
+	height: 200px;
+	
+}
+
+</style>
