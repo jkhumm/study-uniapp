a9fb0c44081a89f552e764c5869e5f2c0ebd7650
humingming <1158139789@qq.com>
2024-05-07 23:14:55 +0800

    #update

---
+++ components/life-components/life-components.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		组件
+	</view>
+</template>
+
+<script setup>
+	import {
+		onUnmounted
+	} from 'vue';
+
+	onUnmounted(() => {
+		console.log("组件被卸载")
+	})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index/demo3/life_demo/life_demo.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="layout">
+		<scroll-view scroll-y="true" class="scroll" ref="scroll">
+			<view></view>
+		</scroll-view>
+
+		<life-components v-if="show"></life-components>
+
+
+	</view>
+</template>
+
+<script setup>
+	import {
+		onMounted,
+		ref
+	} from 'vue';
+
+	const show = ref(true)
+	const scroll = ref(null)
+
+	// 挂载生命周期
+	onMounted(() => {
+		console.log(scroll.value)
+	})
+
+	setTimeout(() => {
+		show.value = false
+	}, 2000)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
