3a4aed2bfd9bbb4755d4ca999b71dd9365b462c6
humingming <1158139789@qq.com>
2024-05-08 23:59:07 +0800

    #update

---
+++ components/demo-child/demo-child.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="out">
+		子组件count值 {{count}}
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from 'vue';
+
+
+	const count = ref(100)
+
+	const updateCount = function() {
+		count.value++;
+	}
+
+	// 导出count到父页面
+	defineExpose({
+		youObj: count,
+		name: "明哥",
+		age: 11,
+		updateCount
+	})
+</script>
+
+<style lang="scss" scoped>
+	.out {
+		padding: 20px;
+		background: #eee;
+	}
+</style>
---
+++ pages/index/demo3/expose_demo/expose_demo.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="layout">
+		<demo-child ref="child">
+		</demo-child>
+		<view>-----------</view>
+		<button @click="update">点击修改子元素</button>
+
+	</view>
+</template>
+
+<script setup>
+	import {
+		onMounted,
+		ref
+	} from 'vue';
+
+	const child = ref(null)
+
+
+
+	onMounted(() => {
+		console.log(child.value)
+	})
+
+	const update = function() {
+		// 调用子组件暴露的方法
+		child.value.updateCount()
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
